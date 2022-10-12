17b67f74bec9f71d2d8a1382c40197169f50e7c2
Mahmod Hassan <mahmodhasan7788@gmail.com>
2022-10-12 20:54:56 +0600

    live link added to readmi file

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # QUIZ TEST
 
-### `Live Site` : [http://localhost:3000](http://localhost:3000)
+### `Live Site` : [http://localhost:3000](https://dapper-queijadas-08f792.netlify.app/)
 * Feature of this Site :
    * This is a Educational Website. you can justify you knowledge by taking a QuizExam in this site.
    * this is a single page application.
--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Col } from 'react-bootstrap';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Options = ({ option, correctAnswer }) => {
     console.log(option);
+    const [isValid, setIsValid] = useState('abc');
     const handleQuizOptionButton = (e) => {
-        console.log(e.target.innerText);
-        if (e.target.innerText === correctAnswer) {
+        let optionValue = e.target.innerText;
+        if (optionValue === correctAnswer) {
             right();
-
+            setIsValid(optionValue);
         } else {
             wrong();
+            setIsValid('');
         }
     }
     const right = () => toast.success('correct! woow!!')
     const wrong = () => toast.error(`Your answer is wrong`)
     return (
         <Col>
-            <button onClick={handleQuizOptionButton} className='w-100 bg-success border-0 p-1 fs-5 text-light'>{option}</button>
+            {
+                isValid === correctAnswer ?
+                    <button onClick={handleQuizOptionButton} className='w-100 bg-success  border-0 p-1 fs-5 text-light'>{option}</button>
+                    :
+                    <button onClick={handleQuizOptionButton} className='w-100 bg-secondary  border-0 p-1 fs-5 text-light'>{option}</button>
+            }
             <ToastContainer />
         </Col>
     );
